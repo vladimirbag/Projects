3a2c1214b59b665ed34310090b67bd035ec7caa9
Vova38 <vova27n@gmail.com>
2024-09-21 14:23:02 +0300

    Add firs hw

--- hw_3_1.py
+++ hw_3_1.py
@@ -2,8 +2,12 @@ from datetime import datetime
 
 def get_days_from_today(date_string):
 
-    # перетворюємо рядок в дату
-    date = datetime.strptime(date_string, '%Y-%m-%d').date()
+    try:
+        # перетворюємо рядок в дату
+        date = datetime.strptime(date_string, '%Y-%m-%d').date()
+    except ValueError:
+        # Якщо формат дати неправильний
+        return "Error: Invalid date format. Please use 'YYYY-MM-DD'."
 
     # відображаємо поточну дату 
     current_date = datetime.today().date()
@@ -18,3 +22,4 @@ def get_days_from_today(date_string):
 
 print(get_days_from_today("2024-09-20"))
 print(get_days_from_today("2024-10-20"))
+print(get_days_from_today("2024/10/20"))
